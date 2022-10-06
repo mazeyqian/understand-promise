82109c524dc411afc2232426b8de598351652646
钱程 <mazey@mazey.net>
2022-10-06 10:49:52 +0800

    chore: update ignore

--- examples/3.1-1.js
+++
@@ -1,6 +0,0 @@
-methods: {
-  comLogin() {
-    // Other Code...
-    State.quickLogin(callback); // 1
-  },
-},
--- examples/3.1-3.js
+++
@@ -1,6 +0,0 @@
-methods: {
-  async comLogin() {
-    const userInfo = await State.quickLogin(); // 1
-    // Other Code...
-  },
-},
